23f7f8cfde23f40764a1637a05cecc7fd4476feb
TheAllSeer <itamar1902@gmail.com>
2025-07-05 20:59:18 +0300

    weekly navigator done

--- lifting-log/components/WeekNavigator.tsx
+++ lifting-log/components/WeekNavigator.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import {Text, View, ViewStyle} from 'react-native';
-import {StyleSheet} from 'react-native';
+import {Text, View, ViewStyle, Pressable, StyleSheet} from 'react-native';
 import styles from '@/styles/general';
 import { Ionicons } from '@expo/vector-icons';
-import { Pressable } from 'react-native';
 
 
 
 const WeekNavigator = ()=>{
     return <View 
-    style={[styles.base, styles.homeCard, wnStyles]}
-    >
-        <Ionicons name='chevron-back-outline' size={24} color={"#ffffff"} ></Ionicons>
-        <View>
-            <Text style={{color:"#f2f2f2"}}>
-                Week 14.6 - 21.6
-            </Text>
+        style={[styles.base, styles.homeCard, wnStyles]}
+        >
+            <Pressable>
+            {(({pressed})=>(
+                <Ionicons name='chevron-back-outline' size={24} color={pressed ? "#ff8c00" : "#ffffff"} ></Ionicons>
+            ))}
+            </Pressable>
+            <View>
+                <Text style={{color:"#f2f2f2"}}>
+                    Week 14.6 - 21.6
+                </Text>
+            </View>
+            <Pressable>
+            {(({pressed})=>(
+                <Ionicons name='chevron-forward-outline' size={24} color={pressed ? "#ff8c00" : "#ffffff"} ></Ionicons>
+            ))}
+            </Pressable>
         </View>
-        <Ionicons name='chevron-forward-outline' size={24} color={"#ffffff"} ></Ionicons>
-    </View>
-
 };
 
 const wnStyles:ViewStyle = {
